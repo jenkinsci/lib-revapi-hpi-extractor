d4f83cf6572ea8aee20e491a453f55dc9e192eb6
Ulli Hafner <ullrich.hafner@gmail.com>
2019-03-05 08:33:53 +0100

    Added license.

--- src/main/java/io/jenkins/tools/revapi/HpiJarExtractor.java
+++ src/main/java/io/jenkins/tools/revapi/HpiJarExtractor.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2014-2019 Lukas Krejci
- * and other contributors as indicated by the @author tags.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.jenkins.tools.revapi;
 
 import java.io.BufferedOutputStream;
@@ -44,6 +28,8 @@ import org.slf4j.LoggerFactory;
  * analysis that are part of the [plugin-artifactId].jar file in the WEB-INF/lib folder.
  *
  * <p><b>Extension ID:</b> {@code hpi}
+ *
+ * @author Ullrich Hafner
  */
 public class HpiJarExtractor implements JarExtractor {
     private static final Logger LOG = LoggerFactory.getLogger(HpiJarExtractor.class);
